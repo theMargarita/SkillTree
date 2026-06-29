543965988673cdc6fa08bf4455aa66d8dc47d6df
theMargarita <margotvoistinova@hotmail.se>
2026-06-29 21:40:29 +0200

    Refactor SkillTreeDbContext into interface and class

--- Infrastructure/Data/SkillTreeDbContext.cs
+++ Infrastructure/Data/SkillTreeDbContext.cs
@@ -1,7 +1,31 @@
-﻿namespace Infrastructure.Data
-{
-    public class SkillTreeDbContext
-    {
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
 
+namespace Infrastructure.Data
+{
+    public interface ISkillTreeDbContext
+    {
+        DbSet<SkillBorad> SkillBoard { get; set; }
+        DbSet<Skills> Skills { get; set; }
+        DbSet<ProgressEntries> ProgressEntries { get; set; }
+        DbSet<SkillConnections> SkillConnections { get; set; }
+        DbSet<SubSkills> SubSkills { get; set; }
+        DbSet<User> User { get; set; }
+    }
+
+
+    public class SkillDbContext : DbContext, ISkillTreeDbContext
+    {
+        public SkillDbContext(DbContextOptions<SkillDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<SkillBorad> SkillBoard { get; set; }
+        public DbSet<Skills> Skills { get; set; }
+        public DbSet<ProgressEntries> ProgressEntries { get; set; }
+        public DbSet<SkillConnections> SkillConnections { get; set; }
+        public DbSet<SubSkills> SubSkills { get; set; }
+        public DbSet<User> User { get; set; }
     }
 }
