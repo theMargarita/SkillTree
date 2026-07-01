658aed938797cac2a7a5bedaff09335b41e9a342
theMargarita <margotvoistinova@hotmail.se>
2026-07-01 18:15:02 +0200

    Enhance SkillTreeDbContext with Cascade Delete

--- Infrastructure/Data/SkillTreeDbContext.cs
+++ Infrastructure/Data/SkillTreeDbContext.cs
@@ -5,6 +5,7 @@ namespace Infrastructure.Data
 {
     public interface ISkillTreeDbContext
     {
+        //private
         DbSet<SkillBorad> SkillBoard { get; set; }
         DbSet<Skills> Skills { get; set; }
         DbSet<ProgressEntries> ProgressEntries { get; set; }
@@ -20,12 +21,35 @@ namespace Infrastructure.Data
         {
 
         }
-
         public DbSet<SkillBorad> SkillBoard { get; set; }
         public DbSet<Skills> Skills { get; set; }
         public DbSet<ProgressEntries> ProgressEntries { get; set; }
         public DbSet<SkillConnections> SkillConnections { get; set; }
         public DbSet<SubSkills> SubSkills { get; set; }
         public DbSet<User> User { get; set; }
+
+        //to "default" to cascade delete behavior
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var fk in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+            {
+                //removes a parent entity automatically triggers deletion of its related child
+                fk.DeleteBehavior = DeleteBehavior.Cascade;
+            }
+
+            foreach(var e in modelBuilder.Model.GetEntityTypes())
+            {
+                e.SetTableName(e.GetTableName()!.ToLower());
+            }
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+        }
     }
+
 }
