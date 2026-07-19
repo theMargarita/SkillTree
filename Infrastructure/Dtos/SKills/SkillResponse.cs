20063e98453bed812256a20d32d80491b0f0dfa1
theMargarita <margotvoistinova@hotmail.se>
2026-07-19 11:13:44 +0200

    Add SkillRequest and SkillResponse DTOs

---
+++ Infrastructure/Dtos/SKills/SkillResponse.cs
@@ -0,0 +1,21 @@
+﻿using Domain;
+
+namespace Infrastructure.Dtos.SKills
+{
+    public record SkillResponse
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public decimal PositionX { get; set; }
+        public decimal PositionY { get; set; }
+        public int RequiredSubSkillCount { get; set; }
+        public string? Color { get; set; }
+        public string? Icon { get; set; }
+        public int Shape { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+        public DateTimeOffset UpdatedAt { get; set; }
+    }
+}
