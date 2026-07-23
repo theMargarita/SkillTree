f3d90809d6b7f9d1183365e975b66e15eb4dbc8e
theMargarita <margotvoistinova@hotmail.se>
2026-07-23 21:22:02 +0200

    feat: new migration from changing and adding an enum for shape so simplify for frontend

---
+++ Infrastructure/Migrations/20260723192058_updateSkillsModel.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateSkillsModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Height",
+                table: "skills");
+
+            migrationBuilder.DropColumn(
+                name: "Width",
+                table: "skills");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Height",
+                table: "skills",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Width",
+                table: "skills",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
