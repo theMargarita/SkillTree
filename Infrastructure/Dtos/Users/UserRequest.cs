75c8197b3e49d70862c2b4acf5a19726e3f36f18
theMargarita <margotvoistinova@hotmail.se>
2026-08-20 10:17:53 +0200

    Add UserRequest and UserResponse DTO classes

---
+++ Infrastructure/Dtos/Users/UserRequest.cs
@@ -0,0 +1,12 @@
+﻿using DataAnnotationsExtensions;
+
+namespace Infrastructure.Dtos.Users
+{
+    public class UserRequest
+    {
+        public string Name { get; set; } = string.Empty;
+        [Email]
+        public string UserEmail { get; set; } = string.Empty;
+        public string HashPassward { get; set; } = string.Empty;
+    }
+}
---
+++ Infrastructure/Dtos/Users/UserResponse.cs
@@ -0,0 +1,13 @@
+﻿using DataAnnotationsExtensions;
+
+namespace Infrastructure.Dtos.Users
+{
+    public class UserResponse
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        [Email]
+        public string UserEmail { get; set; } = string.Empty.ToString();
+        public DateTimeOffset CreatedAt { get; set; }
+    }
+}
