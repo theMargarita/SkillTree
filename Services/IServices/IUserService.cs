dbd447e857b7faae5932ab01033a66c4863d34cb
theMargarita <margotvoistinova@hotmail.se>
2026-08-20 10:23:08 +0200

    Add IUserService interface and update dependencies

---
+++ Services/IServices/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using Infrastructure.Dtos.Users;
+
+namespace Services.IServices
+{
+    public interface IUserService
+    {
+        Task<UserResponse> Create(UserRequest request);
+        Task<UserResponse> Update(Guid id, UserRequest request);
+        Task<bool> Delete(Guid id);
+        Task<UserResponse> GetAll();
+        Task<List<UserResponse>> GetById(string id);
+    }
+}
