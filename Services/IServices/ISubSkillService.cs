2c651f26b631fc6a20cbac2c86de17aac3ffdefe
theMargarita <margotvoistinova@hotmail.se>
2026-08-28 10:03:21 +0200

    Add ISubSkillService interface with sub-skill operations

---
+++ Services/IServices/ISubSkillService.cs
@@ -0,0 +1,16 @@
+﻿using Infrastructure.Dtos.SubSkill;
+
+namespace Services.IServices
+{
+    public interface ISubSkillService
+    {
+        Task<List<SubSkillResponse>> GetAllBySkill(Guid skillId);
+        Task<List<SubSkillResponse>> GetCompleted();
+        Task<SubSkillResponse> Create(SubSkillRequest  request);
+        Task<SubSkillResponse> Update(SubSkillRequest request);
+        Task<bool> Delete(Guid subskillId); //fetch via GetByIdBasic, remove it
+        Task<bool> MarkAsComplete(SubSkillRequest request);
+        Task<bool> MarkAsIncomplete(SubSkillRequest request); //not sure about this
+        Task<List<SubSkillResponse>> GetWithProgress();
+    }
+}
