dd14379b9c57164a160ab7b44ea7b36a169d72ab
theMargarita <margotvoistinova@hotmail.se>
2026-08-29 16:34:25 +0200

    Make CompletedAt nullable in SubSkills class

--- Domain/SubSkills.cs
+++ Domain/SubSkills.cs
@@ -13,7 +13,7 @@
         public int OrderIndex { get; set; }
         public string? Color { get; set; }
         public bool IsComplete { get; set; } = false;
-        public DateTimeOffset CompletedAt { get; set; }
+        public DateTimeOffset? CompletedAt { get; set; }
 
         public Skills Skills { get; set; } = null!;
         public ICollection<ProgressEntries> ProgressEntries = new List<ProgressEntries>();
