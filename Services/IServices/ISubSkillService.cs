10a33ffcd6c8195af70ca8fc6c4445244c4fe779
theMargarita <margotvoistinova@hotmail.se>
2026-08-29 16:35:10 +0200

    Refactor ISubSkillService method signatures

--- Services/IServices/ISubSkillService.cs
+++ Services/IServices/ISubSkillService.cs
@@ -5,12 +5,12 @@ namespace Services.IServices
     public interface ISubSkillService
     {
         Task<List<SubSkillResponse>> GetAllBySkill(Guid skillId);
-        Task<List<SubSkillResponse>> GetCompleted();
+        Task<List<SubSkillResponse>> GetCompleted(Guid skillId);
         Task<SubSkillResponse> Create(SubSkillRequest  request);
-        Task<SubSkillResponse> Update(SubSkillRequest request);
-        Task<bool> Delete(Guid subskillId); //fetch via GetByIdBasic, remove it
-        Task<bool> MarkAsComplete(SubSkillRequest request);
-        Task<bool> MarkAsIncomplete(SubSkillRequest request); //not sure about this
-        Task<List<SubSkillResponse>> GetWithProgress();
+        Task<SubSkillResponse> Update(SubSkillRequest request, Guid subskillId);
+        Task<bool> Delete(Guid subskillId); //fetch via GetById, remove it
+        Task<bool> MarkAsComplete(Guid subskillId);
+        Task<bool> MarkAsIncomplete(Guid subskillId); //not sure about this
+        Task<List<SubSkillResponse>> GetWithProgress(Guid skillId);
     }
 }
