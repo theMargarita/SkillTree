69e72ed2a738b4a8f3494c9ebe10d0188f27cb89
theMargarita <margotvoistinova@hotmail.se>
2026-09-25 22:40:05 +0200

    Add IStatusService interface and SkillStatus enum

---
+++ Services/IServices/IStatusService.cs
@@ -0,0 +1,18 @@
+﻿using Domain;
+
+namespace Services.IServices
+{
+    public interface IStatusService
+    {
+        Dictionary<Guid, SkillStatus> ComputeStatuses(
+            IEnumerable<Skills> skills,
+            IEnumerable<SkillConnections> connections,
+            IReadOnlyDictionary<Guid, int> completedSubSkillCounts);
+    }
+    public enum SkillStatus
+    {
+        Locked,
+        Unlocked,
+        Completed
+    }
+}
