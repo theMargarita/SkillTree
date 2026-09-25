5167d98bf2944118eafb3695ca81559a04fcfcf3
theMargarita <margotvoistinova@hotmail.se>
2026-09-25 22:41:08 +0200

    add service for skill status

---
+++ Services/Services/StatusService.cs
@@ -0,0 +1,13 @@
+﻿using Domain;
+using Services.IServices;
+
+namespace Services.Services
+{
+    public class StatusService : IStatusService
+    {
+        public Dictionary<Guid, SkillStatus> ComputeStatuses(IEnumerable<Skills> skills, IEnumerable<SkillConnections> connections, IReadOnlyDictionary<Guid, int> completedSubSkillCounts)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
